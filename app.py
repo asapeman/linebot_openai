e8ebf98c68a16ad6fa6fe433dbeb96267ac47496
asapeman <53456333+asapeman@users.noreply.github.com>
2026-08-02 20:44:36 +0800

    Update app.py

--- app.py
+++ app.py
@@ -171,13 +171,14 @@ def handle_message(event):
             line_bot_api.reply_message(event.reply_token, TextSendMessage('我屬於老實人\U0001F495'))
         elif '屬' in msg and '於' in msg:
             line_bot_api.reply_message(event.reply_token, TextSendMessage('我屬於老實人\U0001F495'))
+        elif '八月二號' in msg or '8/2' in msg:
+            line_bot_api.reply_message(event.reply_token, TextSendMessage('隔天八月三號是我一生摯愛的生日\U0001F497'))
         
     
 
     elif msg == '挖':
         line_bot_api.reply_message(event.reply_token, StickerSendMessage(package_id=446, sticker_id=2011))
-    elif '八月二號' in msg or '8/2' in msg:
-        line_bot_api.reply_message(event.reply_token, TextSendMessage('隔天八月三號是我一生摯愛的生日\U0001F497'))
+    
             
 
 @handler.add(PostbackEvent)

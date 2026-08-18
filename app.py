30bfa87b96a57f4152d68d48be775db55d58a534
asapeman <53456333+asapeman@users.noreply.github.com>
2026-08-18 15:07:01 +0800

    Update app.py

--- app.py
+++ app.py
@@ -173,13 +173,13 @@ def handle_message(event):
             line_bot_api.reply_message(event.reply_token, TextSendMessage('我屬於老實人\U0001F495'))
         elif '八月二號' in msg or '8/2' in msg:
             line_bot_api.reply_message(event.reply_token, TextSendMessage('隔天八月三號是我一生摯愛的生日\U0001F497'))
+        elif '七夕' in msg or '農曆7/7' in msg or '7夕' in msg:
+            line_bot_api.reply_message(event.reply_token, [TextSendMessage('七夕的郭郎是一夜七次郎\U00002764\uFE0F'),StickerSendMessage(package_id=6362, sticker_id=11087934)])
         
     
 
     elif msg == '挖':
         line_bot_api.reply_message(event.reply_token, StickerSendMessage(package_id=446, sticker_id=2011))
-    elif '七夕' in msg or '農曆7/7' in msg or '7夕' in msg:
-        line_bot_api.reply_message(event.reply_token, [TextSendMessage('七夕的郭郎是一夜七次郎\U00002764\uFE0F'),StickerSendMessage(package_id=6362, sticker_id=11087934)])
     
             
 
